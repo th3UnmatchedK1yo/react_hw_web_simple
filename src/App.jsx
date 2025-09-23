fc3a73696f6b7c6b04838620f78a17cf778da266
Zero Y One K <markbillhv@gmail.com>
2025-09-23 09:29:42 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        fontSize: "2rem",
+        textAlign: "center",
+        marginTop: "20%",
+      }}
+    >
+      Hello World
+    </div>
+  );
+}
+
+export default App;
